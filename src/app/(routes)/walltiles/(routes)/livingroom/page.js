7ae9f1a59ce56ced4706b02e0bbda615f6b2623e
heismyke <81974207+heismyke@users.noreply.github.com>
2024-07-06 20:58:13 +0100

    justiles

--- src/app/(routes)/walltiles/(routes)/livingroom/page.js
+++ src/app/(routes)/walltiles/(routes)/livingroom/page.js
@@ -6,7 +6,7 @@ const LivingRoom = () => {
     <div className='bg-[#272727]'>
            <div className='p-3 md:p-10 lg:p-10 xl:pt-[50px] xl:px-[100px] '>
       <h1 className='text-2xl  text-[#D0C9C0]'>Living Room</h1>
-      <p className='text-lg mt-4 xl:text-xl xl:leading-[40px] text-[#D0C9C0] leading-[40px] '>If you’re updating your living room, choosing the right flooring is probably the most important decision you can make; after all, the floor sets the foundations for the rest of your space.
+      <p className='text-lg mt-4 xl:text-lg xl:leading-[40px] text-[#D0C9C0] leading-[40px] '>If you’re updating your living room, choosing the right flooring is probably the most important decision you can make; after all, the floor sets the foundations for the rest of your space.
       We’ve got a huge range of living room floor tiles in an array of shapes, sizes and style - so whatever your taste, you’re bound to find something that suits. And our floor tiles don’t just look great, they’re also highly practical and perfect to use with underfloor heating.</p>
    
           </div>
--- src/components/wall-tiles/Navigation.jsx
+++ src/components/wall-tiles/Navigation.jsx
@@ -1,17 +1,35 @@
+"use client"
+
+
 import React from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 const Navigation = () => {
+  const router = useRouter()
+
+  const links = [
+    { href: "/walltiles/bathroom-wall-tiles", label: "bathroom wall tiles" },
+    { href: "/walltiles/kitchen-wall-tiles", label: "kitchen wall tiles" },
+    { href: "/walltiles/outdoor-wall-tiles", label: "outdoor wall tiles" },
+    { href: "/walltiles/livingroom", label: "living room" },
+    { href: "/walltiles/bathroom", label: "bathroom" },
+    { href: "/walltiles/commercial-space-wall-tiles", label: "commercial space wall tiles" },
+  ];
   return (
-      <div>
-           <div className='w-[90%] py-5 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-5 mx-auto'>
-        <Link href="/walltiles/bathroom-wall-tiles" className=' flex items-center border-[#5F7161] border-[0.1px] justify-center py-3 text-[#D0C9C0] hover:bg-[#5F7161] active:bg-[#5F7161]'>bathroom wall tiles</Link>
-        <Link href="/walltiles/kitchen-wall-tiles" className='border-[#5F7161] border-[0.1px] flex items-center justify-center py-3 text-[#D0C9C0] hover:bg-[#5F7161] active:bg-[#5F7161]'>kitchen wall tiles</Link>
-        <Link href="/walltiles/outdoor-wall-tiles" className='border-[#5F7161] border-[0.1px] flex items-center justify-center py-3 text-[#D0C9C0] hover:bg-[#5F7161] active:bg-[#5F7161]'>outdoor wall tiles</Link>
-        <Link href="/walltiles/livingroom" className='border-[#5F7161] border-[0.1px] flex items-center justify-center py-3 text-[#D0C9C0] hover:bg-[#5F7161] active:bg-[#5F7161]'>living room</Link>
-        <Link href="/walltiles/bathroom" className='border-[#5F7161] border-[0.1px] flex items-center justify-center  text-[#D0C9C0] hover:bg-[#5F7161] active:bg-[#5F7161]'>bathroom</Link>
-        <Link href="/walltiles/commercial-space-wall-tiles" className='border-[#5F7161] border-[0.1px] flex items-center justify-center lg:py-3 pl-2 text-[#D0C9C0] hover:bg-[#5F7161] active:bg-[#5F7161]'>commercial space wall tiles</Link>
-      </div>
+    <div className='w-[90%] py-5 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-5 mx-auto'>
+      {links.map(link => (
+        <Link
+          href={link.href}
+          key={link.href}
+          passHref
+          className={`flex items-center border-[#5F7161] border-[0.1px] justify-center py-3 text-[#D0C9C0] ${
+            router.pathname === link.href ? 'bg-[#5F7161]' : 'hover:bg-[#5F7161]'
+          }`}
+        >
+          {link.label}
+        </Link>
+      ))}
     </div>
   )
 }
